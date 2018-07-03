d32000eb9acd6e2d8a3f55676466b6110f00f8f0
zarcli <zarcli@hotmail.com>
2018-07-03 23:15:54 +0800

    add

---
+++ Crypto/Base58.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace WisedomChain_Core.Crypto
+{
+    public static class Base58
+    {
+        /// <summary>
+        /// alphabet of base58
+        /// </summary>
+        public const string Alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+        /// <summary>
+        /// base58 decode
+        /// </summary>
+        public static byte[] Decode(string input)
+        {
+            BigInteger bi = BigInteger.Zero;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int index = Alphabet.IndexOf(input[i]);
+                if (index == -1)
+                    throw new FormatException();
+                bi += index * BigInteger.Pow(58, input.Length - 1 - i);
+            }
+            byte[] bytes = bi.ToByteArray();
+            Array.Reverse(bytes);
+            bool stripSignByte = bytes.Length > 1 && bytes[0] == 0 && bytes[1] >= 0x80;
+            int leadingZeros = 0;
+            for (int i = 0; i < input.Length && input[i] == Alphabet[0]; i++)
+            {
+                leadingZeros++;
+            }
+            byte[] tmp = new byte[bytes.Length - (stripSignByte ? 1 : 0) + leadingZeros];
+            Array.Copy(bytes, stripSignByte ? 1 : 0, tmp, leadingZeros, tmp.Length - leadingZeros);
+            return tmp;
+        }
+
+        /// <summary>
+        /// base58 encode
+        /// </summary>
+        public static string Encode(byte[] input)
+        {
+            BigInteger value = new BigInteger(new byte[1].Concat(input).Reverse().ToArray());
+            StringBuilder sb = new StringBuilder();
+            while (value >= 58)
+            {
+                BigInteger mod = value % 58;
+                sb.Insert(0, Alphabet[(int)mod]);
+                value /= 58;
+            }
+            sb.Insert(0, Alphabet[(int)value]);
+            foreach (byte b in input)
+            {
+                if (b == 0)
+                    sb.Insert(0, Alphabet[0]);
+                else
+                    break;
+            }
+            return sb.ToString();
+        }
+    }
+}
