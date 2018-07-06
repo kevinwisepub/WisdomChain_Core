19516c06ebc422b75039f9d61fae5f80f77b104d
zarcli <zarcli@hotmail.com>
2018-07-06 15:06:49 +0800

    modify namespace

--- LevelDB/WriteOptions.cs
+++ LevelDB/WriteOptions.cs
@@ -31,7 +31,7 @@
 //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 using System;
 
-namespace WisedomChain_Core.LevelDB
+namespace WisdomCore.LevelDB
 {
     /// <summary>
     /// Write options
