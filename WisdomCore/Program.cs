cd6d45261227602fa376bb723f13a1634ed60835
zarcli <zarcli@hotmail.com>
2018-07-06 15:32:25 +0800

    add test project

---
+++ WisdomCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WisdomCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //startup service
+
+
+            //init wallet
+
+
+            Console.WriteLine("Wisedom startup successfully");
+        }
+    }
+}
