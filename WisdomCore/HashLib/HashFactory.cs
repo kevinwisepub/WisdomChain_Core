c6f00fe08a68e721bb9ca0a4b0fd600011875980
zarcli <zarcli@hotmail.com>
2018-07-11 22:16:44 +0800

    add sha3

---
+++ WisdomCore/HashLib/HashFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WisdomCore.HashLib
+{
+    public static class HashFactory
+    {
+        public static class Crypto
+        {
+            public static class SHA3
+            {
+                public static IHash CreateKeccak()
+                {
+                    return new HashLib.Crypto.SHA3.Keccak512();
+                }
+            }
+        }
+    }
+}
